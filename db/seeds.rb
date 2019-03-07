3e0ad1514b7057681493c682132b4f067c76f17f
Anders <kiss7medical@gmail.com>
2019-03-07 15:21:58 +0100

    seeds.rb complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+# ############ OPTIONAL #############
+
+require 'open-uri' # Needed to open web urls
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+to_add = JSON.parse(open(url).read)
+
+drinks = to_add['drinks']
+
+puts drinks
+
+puts 'Creating ingredients...'
+30.times do |n|
+	  ingredients = Ingredient.new(
+	    name:    drinks[n].values[0],
+	  )
+	  ingredients.save!
+  p drinks[n].values[0]
+end
+
+puts 'Finished!'
