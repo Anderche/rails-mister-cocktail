1889058a9e62807317b7dd56f2214eb301cb590d
Anders <kiss7medical@gmail.com>
2019-03-07 12:33:20 +0100

    exercise complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_111216) do
+ActiveRecord::Schema.define(version: 2019_03_07_113239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_03_07_111216) do
   end
 
   create_table "ingredients", force: :cascade do |t|
-    t.string "ingredient"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
