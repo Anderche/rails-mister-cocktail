ac834e0959805321028f4a327dc5f9926111c9a8
Anders <kiss7medical@gmail.com>
2019-03-07 18:31:32 +0100

    backend logic complete

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,12 +16,24 @@ class DosesController < ApplicationController
 
 
 	def destroy
-		@dose = Dose.new(dose_params)
-		@dose.cocktail = Cocktail.find(params[:cocktail_id])
-
+		@cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
 		@dose.destroy
     # no need for app/views/restaurants/destroy.html.erb
-		redirect_to cocktails_path
+		redirect_to cocktail_path(@cocktail)
+		
+
+		######## OR alternate method ########
+
+		# def destroy
+				
+		#     @dose = Dose.find(params[:id])
+		# 		@dose.destroy
+		#     # no need for app/views/restaurants/destroy.html.erb
+		# 		redirect_to cocktail_path(Cocktail.find(params[:cocktail_id]))
+		# end
+
+		
 	end
 
   private
@@ -31,3 +43,4 @@ class DosesController < ApplicationController
   end
 
 end
+
