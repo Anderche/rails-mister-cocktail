3c82c60f4e2fc5ed19599f4d85e1e6f28469b208
Anders <kiss7medical@gmail.com>
2019-03-07 13:05:24 +0100

    validations complete

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,10 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+	# validates :name, uniqueness: true, presence: true
+ 	validates :description, presence: true
+
+	# A dose must have a description, a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique.
+ 	validates :cocktail, uniqueness: { scope: :ingredient }
 end
