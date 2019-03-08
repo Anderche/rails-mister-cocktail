14893dbb9fbee8bcaf30e5d89b8f8d89e3f64e50
Anders <kiss7medical@gmail.com>
2019-03-08 15:23:38 +0100

    updated routes.rb to include a 'root to:' path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'cocktails#home'
+	
+  # PREVENT homepage from being http://localhost:3000/cocktails. Call the Index page as the root directly, as below:
+	root to: 'cocktails#index'
 
-  resources :cocktails do
+	# INDEX would now be a duplicate route so must "hide" from routes.rb by calling "except:" as below:
+  resources :cocktails, except: :index do
   	# resources :doses, only: [:new, :create, :destroy]
   	resources :doses
   end
